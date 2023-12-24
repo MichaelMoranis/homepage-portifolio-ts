a9991f72607022039e8f1743a23ce59b885dae0c
michaelmoranis <michaelmoranesul@gmail.com>
2023-12-24 14:32:27 -0300

    delete styles component projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,26 +1,59 @@
-import ThumbOne from "../../assets/thumb-1.svg"
-import ThumbTwo from "../../assets/thumb-2.svg"
-import "./styles.css"
+import ThumbOne from "@assets/thumb-1.svg";
+import ThumbTwo from "@assets/thumb-2.svg";
+import "./styles.css";
+import { Box, Image, Text } from "@kuma-ui/core";
 
 function Projects() {
-    return (
-        <div className="container">
-            <div className="container-card">
-                <img className="container-card_image" src={ThumbOne} alt="pequena foto" />
-                <div className="container-card_text">
-                    <p className="container-card_text__2">click here to visit</p>
-                    <p className="card_text">css tutorial</p>
-                </div>
-            </div>
-            <div className="container-card">
-                <img className="container-card_image" src={ThumbTwo} alt="pequena foto 02" />
-                <div className="container-card_text">
-                    <p className="container-card_text__1">click here to visit</p>
-                    <p className="card_text">html tutorial</p>
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <Box display="flex" justifyContent="center" gap="2rem" margin="1rem auto">
+      <Box height="301px" width="390px">
+        <Image src={ThumbOne} alt="pequena foto" />
+        <Box bg="#222" padding="10px 15px">
+          <Text
+            color="#fff"
+            paddingBottom="10px"
+            fontSize="10px"
+            fontFamily={["Roboto", "sans-serif"]}
+          >
+            click here to visit
+          </Text>
+          <Text
+            color="#fff"
+            fontWeight="800"
+            fontSize="24px"
+            lineHeight="19px"
+            fontFamily={["Roboto", "sans-serif"]}
+            textTransform="uppercase"
+          >
+            css tutorial
+          </Text>
+        </Box>
+      </Box>
+      <Box height="301px" width="390px">
+        <Image src={ThumbTwo} alt="pequena foto 02" />
+        <Box bg="#222" padding="10px 15px">
+          <Text
+            color="#fff"
+            paddingBottom="10px"
+            fontSize="10px"
+            fontFamily={["Roboto", "sans-serif"]}
+          >
+            click here to visit
+          </Text>
+          <Text
+            color="#fff"
+            fontWeight="800"
+            fontSize="24px"
+            lineHeight="19px"
+            fontFamily={["Roboto", "sans-serif"]}
+            textTransform="uppercase"
+          >
+            html tutorial
+          </Text>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
-export default Projects
+export default Projects;
