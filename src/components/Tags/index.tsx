09b8483459df045e0a2ad2cb3d4aad1b043d53cc
michaelmoranis <michaelmoranesul@gmail.com>
2023-12-24 14:36:48 -0300

    delete styles  tags

--- src/components/Tags/index.tsx
+++ src/components/Tags/index.tsx
@@ -4,18 +4,22 @@ import Css from "@assets/css.svg"
 import NodeJs from "@assets/nodejs.svg"
 import Html from "@assets/html.svg"
 import ReactJs from "@assets/reactjs.svg"
-
-
+import { Box, Image } from '@kuma-ui/core'
 
 function Tags() {
     return (
-      <div className="tags">
-        <img src={Js} alt="logo javascript" />
-        <img src={NodeJs} alt="logo nodejs" />
-        <img src={Html} alt="logo html" />
-        <img src={ReactJs} alt="logo reactjs" />
-        <img src={Css} alt="logo css" />
-      </div>
+      <Box 
+        display="flex"
+        justifyContent="center"
+        gap="60.445px"
+        margin="3rem auto"
+      >
+        <Image src={Js} alt="logo javascript" />
+        <Image src={NodeJs} alt="logo nodejs" />
+        <Image src={Html} alt="logo html" />
+        <Image src={ReactJs} alt="logo reactjs" />
+        <Image src={Css} alt="logo css" />
+      </Box>
     )
 }
 
